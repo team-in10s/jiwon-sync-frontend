c2ef509d976109c125fdf7d775bb0afba8219dbf
jean-lee-999 <jean@in10s.co>
2024-08-05 00:11:46 +0900

    refactor: improved layout of signup page

--- app/app/(auth)/auth/signup/page.tsx
+++ app/app/(auth)/auth/signup/page.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link';
 import SignupForm from './components/signup-form';
 
 export default function Index() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>회원가입</div>
-      <SignupForm />
+    <div className="container flex flex-col items-center justify-center">
+      <h1 className="text-gradient mb-8 text-4xl font-bold md:text-5xl">지원전에 Sync</h1>
 
-      <div>
-        이미 계정이 있으신가요? <Link href="/app/auth/signin">로그인</Link>
-      </div>
-    </main>
+      <SignupForm />
+    </div>
   );
 }
