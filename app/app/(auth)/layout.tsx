28b4d18e9dc8912009748606d44278082d3799b4
jean-lee-999 <jean@in10s.co>
2024-08-05 14:30:40 +0900

    refactor: improved layout of (auth) pages
    - add min-h-screen (100vh) & item center

--- app/app/(auth)/layout.tsx
+++ app/app/(auth)/layout.tsx
@@ -2,9 +2,8 @@ import { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex flex-col items-center justify-between bg-secondary px-4 py-28 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-secondary px-4 py-28 text-white">
       {children}
-      {/* class="flex-grow container mx-auto px-4 pt-20" */}
     </main>
   );
 }
