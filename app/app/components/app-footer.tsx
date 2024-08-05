797975bcc06a871eea3e915e7975a65092d257d0
jean-lee-999 <jean@in10s.co>
2024-08-05 23:12:36 +0900

    chore: update links at app-footer

--- app/app/components/app-footer.tsx
+++ app/app/components/app-footer.tsx
@@ -4,14 +4,19 @@ export default function AppFooter() {
       <div className="container mx-auto px-4">
         <p className="text-gray-300">© 2024 지원전에 Sync. All rights reserved.</p>
         <div className="mt-4 space-x-4">
-          <a href="#" className="text-gray-300 hover:text-white">
+          <a
+            href="https://www.in10s.co/jiwon-sync/terms"
+            target="_blank"
+            className="text-gray-300 hover:text-white"
+            rel="noreferrer"
+          >
             이용약관
           </a>
-          <a href="#" className="text-gray-300 hover:text-white">
+          {/* <a href="#" className="text-gray-300 hover:text-white">
             개인정보처리방침
-          </a>
+          </a> */}
           <a
-            href="http://pf.kakao.com/_xjxkJb6/chat"
+            href="http://pf.kakao.com/_xjxkJbG/chat"
             target="_blank"
             className="text-gray-300 hover:text-white"
             rel="noreferrer"
