4c7def1e1af8e6bf2a8d85512e2c68c14711555b
jean-lee-999 <jean@in10s.co>
2024-08-06 17:54:35 +0900

    feat: add 'disabled' prop to SaveButton

--- app/app/(users)/sync/components/save-button.tsx
+++ app/app/(users)/sync/components/save-button.tsx
@@ -1,9 +1,14 @@
-export default function SaveButton({ isSubmitting }: { isSubmitting: boolean }) {
+type SaveButtonProps = {
+  isSubmitting: boolean;
+  disabled?: boolean;
+};
+
+export default function SaveButton({ isSubmitting, disabled = false }: SaveButtonProps) {
   return (
     <button
       type="submit"
-      className={`btn-gradient mt-8 w-full rounded-full py-2 font-bold text-secondary ${isSubmitting ? 'cursor-not-allowed opacity-50' : ''}`}
-      disabled={isSubmitting}
+      className={`mt-8 w-full rounded-full bg-gradient-to-r from-primary to-gradient-blue py-2 font-semibold text-secondary ${isSubmitting && 'cursor-not-allowed opacity-50'} ${disabled && 'cursor-not-allowed border-gray-400 bg-gray-400/50 bg-none text-white/60'}`}
+      disabled={isSubmitting || disabled}
     >
       {isSubmitting ? '저장하는 중..' : '저장하기'}
     </button>
