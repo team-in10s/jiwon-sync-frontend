5014b689bce1ae73697bf69a2d6fd8bed22d77d8
jean-lee-999 <jean@in10s.co>
2024-08-06 18:25:37 +0900

    style: 지원전에 선택

--- app/app/(users)/sync/components/jiwon-platform.tsx
+++ app/app/(users)/sync/components/jiwon-platform.tsx
@@ -2,11 +2,25 @@ import Link from 'next/link';
 
 export default function JiwonPlatform() {
   return (
-    <div>
+    <div className="">
       <p className="mb-4">
-        ✨지원전에✨ 에서 이력서를 관리하면 모든 채용 플랫폼에 자동으로 동기화됩니다.
+        <span className="shiny-text">지원전에</span> 에서 이력서를 관리하면 모든 채용 플랫폼에
+        자동으로 동기화됩니다.
       </p>
-      <Link href="/app/resume" className="mt-4 rounded-full bg-lime-300 px-4 py-2 text-sm">
+      {/* .btn-gradient {
+    background: linear-gradient(135deg, var(--primary-color), #00ccff);
+    color: var(--secondary-color);
+  } */}
+      {/* <Link
+        href="/app/resume"
+        className="mt-4 rounded-full bg-gradient-to-r from-primary to-[#00ccff] px-4 py-2 text-sm text-black"
+      >
+        이력서 관리하러 가기
+      </Link> */}
+      <Link
+        href="/app/resume"
+        className="mt-4 rounded-full border border-primary px-4 py-2 text-sm"
+      >
         이력서 관리하러 가기
       </Link>
     </div>
