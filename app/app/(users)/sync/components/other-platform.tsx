4985c7ce44fd66d8699225fa204a86d718aaf6fe
jean-lee-999 <jean@in10s.co>
2024-08-06 18:24:53 +0900

    feat: 채용 플랫폼 선택 후 이력서 업로드

--- app/app/(users)/sync/components/other-platform.tsx
+++ app/app/(users)/sync/components/other-platform.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import { saveMainResume } from '@/app/lib/api';
 import { PLATFORM_CONFIG } from '@/app/lib/constants';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import SaveButton from './save-button';
+import { useState } from 'react';
 
 type Inputs = {
-  resumeFileList: FileList;
+  // resumeFile: FileList;
+  resumeFile: File;
 };
 
 export default function OtherPlatform({ selectedPlatform }: { selectedPlatform: string }) {
@@ -13,12 +17,44 @@ export default function OtherPlatform({ selectedPlatform }: { selectedPlatform: 
     register,
     handleSubmit,
     resetField,
+    setValue,
+    getValues,
     formState: { isSubmitting },
   } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+  const [selectedFileName, setSelectedFileName] = useState<string | null>(null);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      setSelectedFileName(file.name);
+      // setValue('resumeFile', e.target.files!); // Update the form value
+      setValue('resumeFile', file); // Update the form value
+    } else {
+      setSelectedFileName(null);
+    }
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files?.[0];
+    if (file) {
+      setSelectedFileName(file.name);
+      // setValue('resumeFile', e.dataTransfer.files!); // Update the form value
+      setValue('resumeFile', file); // Update the form value
+    }
+    setIsDragging(false);
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = async (_) => {
+    const submittedFile = getValues('resumeFile');
+
     const formData = new FormData();
-    formData.append('file', data.resumeFileList[0]);
+    formData.append('platform', selectedPlatform);
+    formData.append('file', submittedFile);
+    formData.append('link', '');
 
     try {
       const res = await saveMainResume(formData);
@@ -30,31 +66,44 @@ export default function OtherPlatform({ selectedPlatform }: { selectedPlatform: 
     } catch (error) {
       toast.error('메인 이력서 저장에 실패했습니다.');
     } finally {
-      resetField('resumeFileList');
+      resetField('resumeFile');
     }
   };
 
   return (
     <div>
       <p>{PLATFORM_CONFIG[selectedPlatform].displayName}에서 다른 채용 플랫폼으로 동기화합니다.</p>
-
-      <div>
-        <p>로그인을 통한 직접 연결은 아직 준비중이예요! 🚧</p>
-        <p>이력서 내보내기를 통해 PDF 파일을 업로드해주세요.</p>
-      </div>
+      <p className="mb-1">이력서 내보내기를 통해 PDF 파일을 업로드해주세요.</p>
+      <p className="mb-4 text-sm text-gray-300">
+        (로그인을 통한 직접 연결은 아직 준비중이에요! 🚧)
+      </p>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label className="block">
-          <span className="block text-sm font-medium text-slate-700">이력서 파일</span>
+        <label
+          htmlFor="resumeFile"
+          className={`mb-3 block w-full cursor-pointer rounded-lg border-2 border-dashed border-primary/60 ${isDragging ? 'bg-primary/10' : 'bg-[#2d2d2d]'} px-4 py-8 text-center transition duration-300 ease-in-out hover:border-primary hover:bg-primary/10`}
+          onDragOver={(e) => {
+            e.preventDefault();
+            setIsDragging(true);
+          }}
+          onDragLeave={() => setIsDragging(false)}
+          onDrop={handleDrop}
+        >
           <input
             type="file"
-            className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-sm shadow-sm placeholder:text-slate-400"
-            {...register('resumeFileList', {
-              required: true,
-            })}
+            id="resumeFile"
+            className="hidden"
+            {...register('resumeFile')}
+            onChange={handleFileChange}
           />
+          <span>
+            {selectedFileName ||
+              `${PLATFORM_CONFIG[selectedPlatform].displayName} 이력서 파일을 여기에 드래그하거나
+            클릭하여 업로드하세요`}
+          </span>
         </label>
-        <SaveButton isSubmitting={isSubmitting} />
+
+        <SaveButton isSubmitting={isSubmitting} disabled={!selectedFileName} />
       </form>
     </div>
   );
