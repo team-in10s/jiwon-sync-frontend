9324c5be235f8c8e794f42b5ef9c23375638e506
jean-lee-999 <jean@in10s.co>
2024-08-10 20:12:31 +0900

    feat: 플랫폼 연결 뷰모델 타입 보강

--- app/app/(users)/sync/components/connected-list.tsx
+++ app/app/(users)/sync/components/connected-list.tsx
@@ -1,68 +1,88 @@
-'use client';
+// 'use client';
 
-// import { connectPlatform } from '../actions';
 import Image from 'next/image';
+import { PlatformName } from '@/app/lib/constants';
+import { PlatformConnectionStatus } from '../types';
+import PlatformConnectButton from './platform-connect-button';
 
 type PropType = {
   list: {
-    platform: string;
-    displayName: string;
-    status: string | null; // TODO:  status 값 타입 따로 잡기 string 말고,
+    platform: PlatformName;
+    displayName?: string;
+    status: PlatformConnectionStatus;
     imageSrc: string | null;
   }[];
 };
 
 export default function ConnectedList({ list }: PropType) {
-  const handleConnect = async (platform: string) => {
-    console.log(platform);
-    // TODO: 플랫폼에 따라 동의 or 핸드폰 인증 필요
-    // 약관 동의부터 보이기: 사람인, 점핏,
-    // 전화번호 인증: 인크루트, 잡코리아, 원티드, 리멤버
-
-    //
-    // await connectPlatform(platform);
-    // 로딩 보이지 말고 일단 optimistic ui update.
-    // 위 통신이 잘 끝나면
-    // statusList.status 를 업데이트 해야 하는데 어떻게 할까
-    // useTransition 훅 참고 -> useTransition is a React Hook that lets you update the state without blocking the UI.
-  };
+  console.log('ConnectedList (server component) rendered');
 
   return (
     <div>
       {list.map((statusList) => {
+        const { platform, imageSrc, displayName, status } = statusList;
         return (
           <div
             className="mb-4 flex items-center justify-between rounded-lg bg-gray-600 p-4"
-            key={statusList.platform}
+            key={platform}
           >
             <div className="flex items-center gap-2">
-              {statusList.imageSrc && (
+              {imageSrc && (
                 <Image
-                  src={statusList.imageSrc}
-                  alt={statusList.displayName}
+                  src={imageSrc}
+                  alt={displayName || ''}
                   width={24}
                   height={24}
                   className="rounded-md"
                 />
               )}
-              <span>{statusList.displayName} </span>
+              <span>{displayName} </span>
             </div>
 
-            {statusList.status === null ? (
-              <button
-                className="btn-gradient rounded-full px-3 py-1 text-sm"
-                onClick={() => {
-                  handleConnect(statusList.platform);
-                }}
-              >
-                연결하기
-              </button>
-            ) : (
-              <span className="text-sm">연결 완료</span>
-            )}
+            <PlatformConnectButton status={status} platform={platform} />
           </div>
         );
       })}
     </div>
   );
 }
+
+// function EmailAuthContent({
+//   selectedPlatform,
+//   onClickConnect,
+// }: {
+//   selectedPlatform: 'saramin' | 'jumpit';
+//   onClickConnect: () => Promise<void>;
+// }) {
+//   return (
+//     <div>
+//       <h3 className="mb-4 text-lg font-bold">{selectedPlatform} 연결</h3>
+//       <p className="mb-2 font-bold">약관 동의</p>
+//       <ul>
+//         {PLATFORM_TERMS[selectedPlatform]?.map((platformTerm) => {
+//           return <li key={platformTerm.title}>{platformTerm.title}</li>;
+//         })}
+//       </ul>
+//       {/* <input type="checkbox" name="" id="" /> */}
+//       <div>
+//         <input type="checkbox" id="agree" name="agree" defaultChecked />
+//         <label htmlFor="agree">모두 동의합니다.</label>
+//       </div>
+//       <button className="bg-green-300 p-2" onClick={onClickConnect}>
+//         연결하기
+//       </button>
+//     </div>
+//   );
+// }
+
+// function PhoneAuthContent({ selectedPlatform }: { selectedPlatform: string | null }) {
+//   return (
+//     <div>
+//       <h3 className="mb-4 text-lg font-bold">{selectedPlatform} 연결</h3>
+//       <p>핸드폰 인증이 필요한 플랫폼</p>
+//       {/* step1: 인증번호 요청 UI */}
+//       {/* step2: 인증번호 입력 UI */}
+//       {/* step3: 약관 동의 UI */}
+//     </div>
+//   );
+// }
