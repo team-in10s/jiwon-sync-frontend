b802c68c5a02bd14a3784dc12c6ca54fb8928655
jean-lee-999 <jean@in10s.co>
2024-08-10 20:14:31 +0900

    (테스트 중) 플랫폼 연결 api route handler 확인 중

--- app/api/platform/connect/[slug]/route.ts
+++ app/api/platform/connect/[slug]/route.ts
@@ -1,8 +1,6 @@
 const baseUrl = process.env.API_BASE_URL;
 
 export async function POST(request: Request, { params }: { params: { slug: string } }) {
-  const slug = params.slug; // platform
-
   const authHeader = request.headers.get('Authorization');
 
   if (!authHeader) {
@@ -12,16 +10,22 @@ export async function POST(request: Request, { params }: { params: { slug: strin
     });
   }
 
+  const slug = params.slug; // platform
+
+  const b = await request.json();
+  const requestBody = b.requestId ? { request_id: b.requestId } : {};
+
   const res = await fetch(`${baseUrl}/platform/connect/${slug}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: authHeader,
     },
+    body: JSON.stringify(requestBody),
   });
 
   const data = await res.json();
-  console.log('data? --------', data);
+  console.log('platform/connect/,,,,data? --------', data);
   // TODO 응답값 타입 잡기
 
   return Response.json(data);
