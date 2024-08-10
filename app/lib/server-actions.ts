d5d3d888602f1d3219f5da3d436159bc7fa4d43e
jean-lee-999 <jean@in10s.co>
2024-08-10 20:13:57 +0900

    (테스트 중) 플랫폼 상태 api 관련 server action 확인 중

--- app/lib/server-actions.ts
+++ app/lib/server-actions.ts
@@ -1,8 +1,12 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
 import { getServerAuth } from './server-auth';
 
 const baseUrl = process.env.API_BASE_URL;
 
-export async function getPlatformStatus() {
+export async function getPlatformStatus(needRevalidate?: 'needRevalidate') {
+  console.log('getPlatformStatus called');
   const { credentials } = getServerAuth();
 
   const response = await fetch(`${baseUrl}/platform/statuses`, {
@@ -10,11 +14,36 @@ export async function getPlatformStatus() {
     headers: {
       Authorization: `Basic ${credentials}`,
     },
+    next: {
+      tags: ['platform-status'], // Add a tag to the cache
+      revalidate: 30, // 30 seconds
+    },
   });
+
+  if (needRevalidate) {
+    revalidatePath('/app/async');
+  }
 
   if (!response.ok) {
     throw new Error('get platform status failed');
   }
 
-  return response.json();
+  const data = await response.json();
+  console.log('getPlatformStatus server action data: ', data);
+  /*
+  
+  [
+    { platform: 'jumpit', status: 'completed' },
+    { platform: 'saramin', status: 'failed' }
+  ]
+  */
+
+  return data;
+}
+
+export async function tryRevalidate() {
+  revalidatePath('/app/sync');
+  revalidatePath('/app/sync', 'layout');
+  revalidatePath('/app/sync', 'page');
+  revalidatePath('/sync');
 }
