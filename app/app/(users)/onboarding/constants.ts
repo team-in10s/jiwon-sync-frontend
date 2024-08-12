7f43ca3fa5bc4dbc237bd79f62d080349e51e070
jean-lee-999 <jean@in10s.co>
2024-08-12 16:23:55 +0900

    chore: 누락된 플랫폼 정보 추가

--- app/app/(users)/onboarding/constants.ts
+++ app/app/(users)/onboarding/constants.ts
@@ -2,9 +2,9 @@
 
 export const HR_PLATFORMS = [
   { id: 'wanted', name: '원티드' },
-  { id: 'jobplanet', name: '잡플래닛' },
-  { id: 'rocketpunch', name: '로켓펀치' },
+  { id: 'jumpit', name: '점핏' },
   { id: 'incruit', name: '인크루트' },
   { id: 'saramin', name: '사람인' },
   { id: 'jobkorea', name: '잡코리아' },
+  { id: 'remember', name: '리멤버' },
 ];
