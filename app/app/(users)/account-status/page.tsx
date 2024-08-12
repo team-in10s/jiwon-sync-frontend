4959e071d507bfc618e597c38bdd55a98d03c281
jean-lee-999 <jean@in10s.co>
2024-08-12 17:29:03 +0900

    chore: 새로운 페이지 Placeholder 추가 (/account-status)

---
+++ app/app/(users)/account-status/page.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>https://jiwon-sync.in10s.co/app/account-status</div>
+    </div>
+  );
+}
