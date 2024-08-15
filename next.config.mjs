fa02894ab706534c7d11c1aa475d3191a91f94c2
jean-lee-999 <jean@in10s.co>
2024-08-15 23:16:54 +0900

    feat: next config 에 프록시 설정
    - localhost:3000~~~ 으로 보내지는 요청을 next가 :8000으로 돌려주도록 설정함

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/platform/:path*',
+        destination: `${process.env.API_BASE_URL}/platform/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
