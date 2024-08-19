e7e091a034c9889a539342fbd9e6f341bd3b52f7
jean-lee-999 <jean@in10s.co>
2024-08-20 00:58:36 +0900

    style: modal style

--- app/app/components/modal.tsx
+++ app/app/components/modal.tsx
@@ -41,11 +41,11 @@ const Modal = ({ isOpen, onClose, children, title }: ModalProps) => {
       onClick={handleBackdropClick}
       role="dialog"
     >
-      <div className="relative w-1/3 rounded-lg bg-white p-6 shadow-lg">
+      <div className="relative w-1/2 rounded-lg bg-white p-6 shadow-lg lg:w-1/3">
         <div className="mb-4 flex items-center justify-between">
           {title && <h2 className="text-xl font-bold">{title}</h2>}
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
-            {/* <X size={24} /> */}x
+          <button onClick={onClose} className="hidden text-gray-500 hover:text-gray-700 md:block">
+            {/* <X size={24} /> */}X
           </button>
         </div>
         <div>{children}</div>
