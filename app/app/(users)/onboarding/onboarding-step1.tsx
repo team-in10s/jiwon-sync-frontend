5efa0abc9f0ea1861b9940f707ee58cdc79a7cd4
jean-lee-999 <jean@in10s.co>
2024-08-20 00:58:20 +0900

    feat: step1 모달 추가

--- app/app/(users)/onboarding/onboarding-step1.tsx
+++ app/app/(users)/onboarding/onboarding-step1.tsx
@@ -1,12 +1,24 @@
 // app/app/(users)/onboarding/onboarding-step1.tsx
 
 import Image from 'next/image';
+import { useState } from 'react';
+import Modal from '../../components/modal';
+import Link from 'next/link';
 
 type Step1Props = {
   onNext: () => void;
 };
 
 export default function OnboardingStep1({ onNext }: Step1Props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="card w-full max-w-2xl p-8">
       {/* heading */}
@@ -35,10 +47,22 @@ export default function OnboardingStep1({ onNext }: Step1Props) {
         <button onClick={onNext} className="btn-gradient rounded-full px-10 py-2 font-semibold">
           1분 만에 계정 생성 후 동기화
         </button>
-        <button className="text-sm text-gray-400 underline underline-offset-2">
+        <button onClick={openModal} className="text-sm text-gray-400 underline underline-offset-2">
           왜 새로운 계정을 만드나요?
         </button>
       </div>
+
+      <Modal isOpen={isModalOpen} onClose={closeModal} title="왜 새로운 계정을 만드나요?">
+        <div className="py-2">
+          <div className="mb-4">
+            지원전에가 여러분의 커리어 비서가 되어 귀찮은 작업을 도와드리기 위해 커리어 전용 계정을
+            만들어요.
+          </div>
+          <Link href="/app/faq#q2" className="underline underline-offset-2">
+            자세히 알아보기
+          </Link>
+        </div>
+      </Modal>
     </div>
   );
 }
