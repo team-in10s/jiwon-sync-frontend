b4050de17c02dd4edcb6ff0cffeb83aa407e97c2
jean-lee-999 <jean@in10s.co>
2024-08-20 18:43:28 +0900

    feat: index 페이지 접근 시 기존 페이지의 랜딩페이지로 보내기

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
+  redirect('https://jiwon-sync.in10s.co/');
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>여긴 개인회원용 랜딩페이지 자리</div>
-    </main>
+    <></>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   <div>여긴 개인회원용 랜딩페이지 자리</div>
+    // </main>
   );
 }
