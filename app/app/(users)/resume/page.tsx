620ba2bfdb7b15f001fdfe1c8bed197ff88d9727
jean-lee-999 <jean@in10s.co>
2024-08-20 09:37:32 +0900

    feat: /resume 페이지에 탈리폼 iframe 추가

--- app/app/(users)/resume/page.tsx
+++ app/app/(users)/resume/page.tsx
@@ -1,7 +1,13 @@
-export default function Index() {
+export default function ResumePage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>https://jiwon-sync.in10s.co/app/resume</div>
+    <div className="flex min-h-screen flex-col items-center justify-between px-24">
+      <iframe
+        src="https://tally.so/r/3E0RA2"
+        width="100"
+        height="100"
+        className="h-dvh w-dvw"
+        title="이력서 관리 랜딩페이지"
+      ></iframe>
     </div>
   );
 }
