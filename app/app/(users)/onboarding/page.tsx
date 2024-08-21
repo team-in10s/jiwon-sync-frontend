a43acfa5552c8c287478725c822030525a105d52
jean-lee-999 <jean@in10s.co>
2024-08-21 19:36:41 +0900

    feat: onboarding 페이지에서 redirect
    - 계정 생성 시도가 1번 이라도 있을 경우 resume로 보냄
    - use case 추가

--- app/app/(users)/onboarding/page.tsx
+++ app/app/(users)/onboarding/page.tsx
@@ -1,8 +1,33 @@
 // app/app/(users)/onboarding/page.tsx
 
 import OnboardingSteps from './onboarding-steps';
+import { checkAndRedirectPlatformStatus } from './use-cases';
 
-export default function Index() {
+export default async function Index() {
+  let error: Error | null = null;
+  try {
+    await checkAndRedirectPlatformStatus();
+  } catch (e) {
+    error = e instanceof Error ? e : new Error('An unknown error occurred');
+    console.error('Error fetching platform status:', error);
+  }
+
+  if (error) {
+    return (
+      <div className="container mx-auto mt-16">
+        <div className="flex justify-center">
+          <h1 className="text-gradient mb-8 text-3xl font-bold">
+            앗, 잠시 후에 다시 시도해 주세요.
+          </h1>
+          <p>
+            에러가 발생했어요. 잠시 후에 다시 시도해 주시거나 카카오톡 고객센터로 문의해 주세요.{' '}
+          </p>
+          <p>{error.message}</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="container mx-auto mt-16">
       <div className="flex justify-center">
--- app/app/(users)/onboarding/use-cases.ts
+++ app/app/(users)/onboarding/use-cases.ts
@@ -1,5 +1,8 @@
 // app/app/(users)/onboarding/use-cases.ts
 
+import { redirect } from 'next/navigation';
+import { getPlatformStatusService } from '../account-status/services';
+import { PlatformStatusItem } from '../account-status/types';
 import { connectPlatformService } from './services';
 import { HrPlatformName, PLATFORM_CONFIG } from '@/app/lib/constants';
 
@@ -19,5 +22,17 @@ export async function connectPlatformUseCase(
   if (PLATFORM_CONFIG[platform]?.authType === 'phone') {
     connectPlatformService(platform, data);
     return;
+  }
+}
+
+export async function checkAndRedirectPlatformStatus(): Promise<void> {
+  const response: PlatformStatusItem[] = await getPlatformStatusService();
+
+  const emptyStatus = response.length === 0; // 계정 생성 시도가 아예 없음
+
+  if (emptyStatus) {
+    redirect('/app/onboarding');
+  } else {
+    redirect('/app/resume');
   }
 }
