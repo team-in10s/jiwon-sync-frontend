3ed2c9b9b46f487c1818838c9024d2dba0807cf0
jean-lee-999 <jean@in10s.co>
2024-08-22 18:43:35 +0900

    쿠키 옵션 임시 제거

--- app/lib/client-auth.ts
+++ app/lib/client-auth.ts
@@ -2,14 +2,13 @@ import Cookies from 'js-cookie';
 import { CREDENTIALS_COOKIE, USER_COOKIE } from './constants';
 import { checkCookieSize } from './utils';
 
-// path=/; domain=.in10s.co; SameSite=None; Secure"
-
 // Cookie options
 const COOKIE_OPTIONS = {
   // ensuring the cookie is only sent over HTTPS connections
   // This ensures that cookies can be set in non-HTTPS environments during development.
   secure: true,
-  domain: process.env.NODE_ENV === 'production' ? '.in10s.co' : undefined,
+  // domain: process.env.NODE_ENV === 'production' ? '.in10s.co' : undefined,
+  // domain: process.env.NODE_ENV === 'production' ? '.in10s.co' : undefined,
   sameSite: 'None' as const, // to allow the cookie to be sent in some cross-site scenarios while still providing some CSRF protection.
   path: '/',
 };
