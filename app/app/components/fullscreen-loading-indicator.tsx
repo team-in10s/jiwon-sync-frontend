933b7148837b22bf813d7508563ce97c48c32a70
jean-lee-999 <jean@in10s.co>
2024-08-22 10:54:41 +0900

    style: 스타일 수정

--- app/app/components/fullscreen-loading-indicator.tsx
+++ app/app/components/fullscreen-loading-indicator.tsx
@@ -22,7 +22,10 @@ export default function FullScreenLoadingIndicator({ message }: { message?: stri
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           ></path>
         </svg>
-        <span className="font-semibold text-gray-700">Loading... {message || ''}</span>
+        <div className="text-center font-semibold text-gray-700">
+          <div>처리 중...</div>
+          <div>{message || ''}</div>
+        </div>
       </div>
     </div>
   );
