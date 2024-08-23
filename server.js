12369979ec328ae4321e2f8b457791732d0b5e9e
jean-lee-999 <jean@in10s.co>
2024-08-23 09:27:13 +0900

    chore: ec2 관련 스크립트 추가

---
+++ server.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// SSL 인증서 파일 경로 설정
+const httpsOptions = {
+  key: fs.readFileSync('./privkey.pem'),
+  cert: fs.readFileSync('./fullchain.pem'),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, '0.0.0.0', (err) => {
+    if (err) throw err;
+    console.log('> Ready on https://0.0.0.0:443');
+  });
+});
