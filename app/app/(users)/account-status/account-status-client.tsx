bd3584ea1c3043e980c1261935dadf44a0ec1839
jean-lee-999 <jean@in10s.co>
2024-08-26 02:44:31 +0900

    feat: sse 수정
    - completed나 failed여도 연결이 계속 유지되는 문제 발견
    - 인스턴스를 ref에 저장해두고 해당 상태면 관련 인스턴스의 close 메소드 호출 후 ref 값도 지워버림
    - 25초 내 응답이 없으면 일단 연결 해제 후 2초 뒤 재시도(최대5회)
    -

--- app/app/(users)/account-status/account-status-client.tsx
+++ app/app/(users)/account-status/account-status-client.tsx
@@ -2,11 +2,16 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
-import { fetchEventSource } from '@microsoft/fetch-event-source';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { getUserAuth } from '@/app/lib/client-auth';
 import PlatformStatusItem from './platform-status-item';
 import { PlatformStatusItem as PlatformStatusItemType } from './types';
+import { HrPlatformName, PLATFORM_CONFIG } from '@/app/lib/constants';
+import Modal from '../../components/modal';
+import FullScreenLoadingIndicator from '../../components/fullscreen-loading-indicator';
+import { EventSourcePolyfill, EventSourcePolyfillInit } from 'event-source-polyfill';
+import EmailPlatformAccount from './email-platform-account';
+import PhonePlatformAccount from './phone-platform-account';
 
 export default function AccountStatusClient({
   initialStatus,
@@ -15,73 +20,210 @@ export default function AccountStatusClient({
 }) {
   const { credentials } = getUserAuth();
   const [platformStatus, setPlatformStatus] = useState<PlatformStatusItemType[]>(initialStatus);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPlatform, setSelectedPlatform] = useState<HrPlatformName>('saramin');
+  const [showsLoadingIndicator, setShowsLoadingIndicator] = useState(false);
+  const eventSourceRef = useRef<{ [key: string]: EventSourcePolyfill | null }>({});
+  const retryAttemptsRef = useRef<{ [key: string]: number }>({});
+  const timeoutIdsRef = useRef<{ [key: string]: number }>({});
 
-  useEffect(() => {
-    const controllers: AbortController[] = [];
+  const setupSSEConnection = useCallback(
+    (platform: string) => {
+      // If there's an existing connection for this platform, don't create a new one
+      if (eventSourceRef.current[platform]) {
+        console.log(`Connection for ${platform} already exists`);
+        return;
+      }
 
-    platformStatus.forEach((platformStat) => {
-      const { platform } = platformStat;
+      // Clear any existing timeout for this platform
+      if (timeoutIdsRef.current[platform]) {
+        clearTimeout(timeoutIdsRef.current[platform]);
+        delete timeoutIdsRef.current[platform];
+      }
 
-      const controller = new AbortController();
-      controllers.push(controller);
+      const url = new URL(`/api/platform/${platform}/status`, window.location.origin);
+      url.searchParams.append('t', Date.now().toString()); // Prevent caching
 
-      // NOTE: next.config.mjs rewrite 설정값 참고
-      // cors 에러 때문에 fetchEventSource에 아래처럼 url 작성하고
-      // nextjs가 http://localhost:3000/api/some/path 로 보내지는 요청을
-      // http://localhost:8000/api/some/path (로컬 기준)으로 전달
-      fetchEventSource(`/api/platform/${platform}/status`, {
-        method: 'GET',
+      console.log(`Setting up new connection for ${platform}`);
+
+      const eventSourceInit: EventSourcePolyfillInit = {
         headers: {
-          Accept: '*/*',
           Authorization: `Basic ${credentials}`,
         },
-        signal: controller.signal,
-        async onopen(response) {
-          if (response.ok && response.headers.get('content-type') === 'text/event-stream') {
-            return; // Everything's good
-          } else if (response.status >= 400 && response.status < 500 && response.status !== 429) {
-            console.error(`HTTP error ${response.status}`);
-            // Throw an error to trigger onerror
-            throw new Error(`HTTP error ${response.status}`);
-          }
-        },
-        onmessage(ev) {
-          // Replace single quotes with double quotes
-          const jsonString = ev.data.replace(/'/g, '"');
-          const data = JSON.parse(jsonString);
-          console.log('Parsed event data:', data);
+        // telling the EventSource to consider the connection inactive
+        // if there's no activity for 25 seconds, and it will attempt to reconnect after this period.
+        heartbeatTimeout: 25000, // 25 seconds in milliseconds
+      };
 
-          // console.log('data? ', data);
-          setPlatformStatus((prevStatus) =>
-            prevStatus.map((p) => (p.platform === platform ? { ...p, status: data.status } : p))
+      eventSourceRef.current[platform] = new EventSourcePolyfill(url.toString(), eventSourceInit);
+
+      eventSourceRef.current[platform]!.onmessage = (event) => {
+        // Reset retry attempts on successful message
+        retryAttemptsRef.current[platform] = 0;
+
+        // Clear any existing timeout for this platform
+        if (timeoutIdsRef.current[platform]) {
+          clearTimeout(timeoutIdsRef.current[platform]);
+          delete timeoutIdsRef.current[platform];
+        }
+
+        const jsonString = event.data.replace(/'/g, '"');
+        const data = JSON.parse(jsonString);
+        console.log(`Received message for ${platform}:`, data);
+
+        setPlatformStatus((prevStatus) =>
+          prevStatus.map((p) => (p.platform === platform ? { ...p, status: data.status } : p))
+        );
+
+        if (data.status === 'completed' || data.status === 'failed') {
+          console.log(`Closing connection for ${platform} due to status: ${data.status}`);
+          eventSourceRef.current[platform]?.close();
+          eventSourceRef.current[platform] = null;
+        }
+      };
+
+      eventSourceRef.current[platform]!.onerror = (error) => {
+        console.error(`SSE error for ${platform}:`, error);
+        eventSourceRef.current[platform]?.close();
+        eventSourceRef.current[platform] = null;
+
+        // Increment retry attempts
+        retryAttemptsRef.current[platform] = (retryAttemptsRef.current[platform] || 0) + 1;
+
+        // Retry connection if less than 5 attempts
+        if (retryAttemptsRef.current[platform] < 5) {
+          console.log(
+            `Retrying connection for ${platform}. Attempt: ${retryAttemptsRef.current[platform]}`
           );
 
-          if (data.status === 'completed' || data.status === 'failed') {
-            controller.abort();
-          }
-        },
-        onclose() {
-          console.log(`Connection closed for ${platform}`);
-        },
-        onerror(err) {
-          console.error(`SSE error for ${platform}:`, err);
-          // You can implement a retry mechanism here if needed
-          //   controller.abort();
-        },
+          // Set a new timeout and store its ID
+          // setTimeout(() => setupSSEConnection(platform), 2000); // Wait 2 second before retrying
+          timeoutIdsRef.current[platform] = window.setTimeout(() => {
+            delete timeoutIdsRef.current[platform];
+            setupSSEConnection(platform);
+          }, 2000);
+        } else {
+          console.log(`Max retry attempts reached for ${platform}`);
+          // Update status to 'failed' after max retries
+          // 근데 필요없어보임..
+          // setPlatformStatus((prevStatus) =>
+          //   prevStatus.map((p) => (p.platform === platform ? { ...p, status: 'failed' } : p))
+          // );
+        }
+      };
+
+      eventSourceRef.current[platform]!.addEventListener('close', (_) => {
+        console.log(`Server closed the connection for ${platform}`);
+        eventSourceRef.current[platform]?.close();
+        eventSourceRef.current[platform] = null;
+
+        // Clear any existing timeout for this platform
+        if (timeoutIdsRef.current[platform]) {
+          clearTimeout(timeoutIdsRef.current[platform]);
+          delete timeoutIdsRef.current[platform];
+        }
       });
+    },
+    [credentials]
+  );
+
+  const stopSSEConnection = useCallback((platform: string) => {
+    if (eventSourceRef.current[platform]) {
+      console.log(`Manually stopping connection for ${platform}`);
+      eventSourceRef.current[platform]?.close();
+      eventSourceRef.current[platform] = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    retryAttemptsRef.current = {}; // Reset retry attempts
+
+    platformStatus.forEach((platformStat) => {
+      if (platformStat.status !== 'completed' && platformStat.status !== 'failed') {
+        setupSSEConnection(platformStat.platform);
+      }
     });
 
+    // Capture the current value of eventSourceRef
+    const currentEventSources = eventSourceRef.current;
+
     return () => {
-      controllers.forEach((controller) => controller.abort());
+      Object.keys(currentEventSources).forEach((platform) => {
+        if (currentEventSources[platform]) {
+          console.log(`Cleaning up connection for ${platform}`);
+          currentEventSources[platform]?.close();
+          currentEventSources[platform] = null;
+        }
+      });
+
+      // Clear all timeouts
+      Object.values(timeoutIdsRef.current).forEach(clearTimeout);
+      timeoutIdsRef.current = {};
     };
-  }, []);
+  }, [platformStatus, setupSSEConnection]);
+
+  const handleConnectClick = (platform: HrPlatformName) => {
+    setSelectedPlatform(platform);
+    setIsModalOpen(true);
+  };
+
+  const handleConnectComplete = useCallback(
+    (platform: HrPlatformName) => {
+      closeModal();
+      stopSSEConnection(platform);
+
+      // Update the platformStatus state immediately
+      setPlatformStatus((prevStatus) =>
+        prevStatus.map((p) => (p.platform === platform ? { ...p, status: 'connecting' } : p))
+      );
+
+      setupSSEConnection(platform);
+    },
+    [setupSSEConnection, stopSSEConnection]
+  );
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
       {platformStatus.map((p) => {
         const { status, platform } = p;
-        return <PlatformStatusItem key={platform} platform={platform} status={status} />;
+        return (
+          <PlatformStatusItem
+            key={platform}
+            platform={platform}
+            status={status}
+            onConnectClick={() => handleConnectClick(platform)}
+          />
+        );
       })}
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title={`${PLATFORM_CONFIG[selectedPlatform]?.displayName}에 계정을 생성합니다`}
+        theme="dark"
+      >
+        {selectedPlatform === 'jumpit' || selectedPlatform === 'saramin' ? (
+          <EmailPlatformAccount
+            platform={selectedPlatform}
+            showLoadingIndicator={setShowsLoadingIndicator}
+            onConnectComplete={handleConnectComplete}
+          />
+        ) : (
+          <PhonePlatformAccount
+            platform={selectedPlatform}
+            showLoadingIndicator={setShowsLoadingIndicator}
+            onConnectComplete={handleConnectComplete}
+          />
+        )}
+      </Modal>
+
+      {showsLoadingIndicator && (
+        <FullScreenLoadingIndicator message="길게는 1분 정도 소요될 수 있어요. 조금만 기다려주세요!" />
+      )}
     </>
   );
 }
