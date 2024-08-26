d1769d92cc4f9d4630db211e0e80ef502489b1c4
jean-lee-999 <jean@in10s.co>
2024-08-26 16:05:49 +0900

    fix: cookie option

--- app/lib/client-auth.ts
+++ app/lib/client-auth.ts
@@ -7,7 +7,7 @@ import { checkCookieSize } from './utils';
 const COOKIE_OPTIONS = {
   // ensuring the cookie is only sent over HTTPS connections
   // This ensures that cookies can be set in non-HTTPS environments during development.
-  secure: true,
+  secure: process.env.NODE_ENV === 'production',
   domain: process.env.NODE_ENV === 'production' ? '.in10s.co' : undefined,
   sameSite: 'None' as const, // to allow the cookie to be sent in some cross-site scenarios while still providing some CSRF protection.
   path: '/',
