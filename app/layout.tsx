617ed59eaeeb083acd138348cd7fd7c542314e3f
jean-lee-999 <jean@in10s.co>
2024-08-26 10:54:36 +0900

    chore: 메타 픽셀, clarity 스크립트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 // app/layout.tsx
 
 import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+import Script from 'next/script';
+
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -8,14 +11,50 @@ export const metadata: Metadata = {
   description: '지원전에 - 커리어에 경쟁력을 더하다',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        {/* Clarity Script */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "n4lt84or2b");
+          `}
+        </Script>
+
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1620393688508165');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+      <body>
+        {children}
+        {/* NoScript part of Meta Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1620393688508165&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
