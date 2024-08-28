95c02330000b30f4fa98e2543d598df0399f7b23
jean-lee-999 <jean@in10s.co>
2024-08-28 09:42:46 +0900

    feat: 메인 로고 클릭시 /app/resume로 이동

--- app/app/faq/layout.tsx
+++ app/app/faq/layout.tsx
@@ -1,22 +1,15 @@
 // app/app/faq/layout.tsx
 
-import Link from 'next/link';
 import { ReactNode } from 'react';
-import Image from 'next/image';
-import mainLogo from '../../main_logo.png';
 import NavLinks from '../(users)/components/nav-links';
+import MainLogoLink from '../components/main-logo-link';
 
 export default function FaqLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <header className="fixed z-50 w-full bg-secondary bg-opacity-40 px-4 py-3 backdrop-blur-md">
         <div className="container mx-auto flex items-center justify-between">
-          <Link href="/app" className="flex gap-1.5">
-            <div className="h-8 w-32">
-              <Image src={mainLogo} alt="지원전에 Sync" unoptimized={true} />
-            </div>
-            <span className="text-gradient text-xl font-bold">Sync</span>
-          </Link>
+          <MainLogoLink />
 
           <NavLinks />
         </div>
