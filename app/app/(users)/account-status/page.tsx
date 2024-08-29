36e1e58a2a43d17eb3e36e046bf3f6369e01b496
jean-lee-999 <jean@in10s.co>
2024-08-29 15:24:30 +0900

    Squashed commit of the following:
    commit b5775c92f4af300250ca569dc31093e7860277a1
    Merge: 2478cbf c3f6ab3
    Author: Jean (Youjin Lee) <jean@in10s.co>
    Date:   Thu Aug 29 15:23:58 2024 +0900
    
        Merge pull request #6 from team-in10s/hotfix
    
        style: 모바일 버전에서 깨짐 최소화
    
    commit c3f6ab3df4c00002da6a3db5653d2b168733b6fc
    Author: jean-lee-999 <jean@in10s.co>
    Date:   Thu Aug 29 15:23:36 2024 +0900
    
        style: 모바일 버전에서 깨짐 최소화
    
    commit 2478cbf5cadce4cdf880973f441ab58a92a7c7e9
    Merge: 6994651 4069d79
    Author: Jean (Youjin Lee) <jean@in10s.co>
    Date:   Thu Aug 29 15:06:16 2024 +0900
    
        Merge pull request #5 from team-in10s/hotfix
    
        style: 모바일 버전에서 깨짐 최소화
    
    commit 4069d7997e9d5c70cf0820dde32bb2519efc3930
    Author: jean-lee-999 <jean@in10s.co>
    Date:   Thu Aug 29 15:05:41 2024 +0900
    
        style: 모바일 버전에서 깨짐 최소화

--- app/app/(users)/account-status/page.tsx
+++ app/app/(users)/account-status/page.tsx
@@ -16,7 +16,7 @@ export default function Index() {
 
       <div className="flex justify-center">
         <div className="w-full max-w-2xl">
-          <div className="mb-6 flex justify-between rounded-lg bg-gray-600 px-8 py-4">
+          <div className="mb-6 flex flex-col justify-between rounded-lg bg-gray-600 p-4 md:flex-row md:px-8">
             <p>
               커리어 전용 계정은 <span className="font-bold text-primary"> one-id</span>로 로그인
               가능해요!
@@ -26,7 +26,7 @@ export default function Index() {
             <DetailButton />
           </div>
 
-          <div className="card w-full max-w-2xl p-8">
+          <div className="card w-full max-w-2xl p-6 md:p-8">
             <h2 className="mb-4 text-xl font-semibold">채용 플랫폼 계정 생성 상태</h2>
 
             <Suspense fallback={<AccountStatusSkeleton />}>
