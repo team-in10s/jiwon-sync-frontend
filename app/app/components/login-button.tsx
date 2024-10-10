1ff47cb84f2a2fb3c99deb413f263e8ab2300c8b
jean-lee-999 <jean@in10s.co>
2024-10-10 16:37:57 +0900

    feat: 모바일 대응 로그인 버튼 수정

--- app/app/components/login-button.tsx
+++ app/app/components/login-button.tsx
@@ -6,8 +6,17 @@ import Link from 'next/link';
 
 export default function LoginButton() {
   return (
-    <Link href="/app/auth/signin" className="btn-gradient rounded-full px-4 py-1.5 text-white">
-      로그인
-    </Link>
+    <>
+      <Link
+        href="/app/auth/signin"
+        className="btn-gradient hidden rounded-full px-4 py-1.5 text-white md:block"
+      >
+        로그인
+      </Link>
+
+      <Link href="/app/auth/signin" className="py-4 md:hidden">
+        로그인
+      </Link>
+    </>
   );
 }
