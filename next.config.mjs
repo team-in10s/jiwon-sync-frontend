f79403d6b92ba16b7cc4ea83a036db7691862aaf
jean-lee-999 <jean@in10s.co>
2024-10-14 19:05:16 +0900

    chore: add highlight

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withHighlightConfig } from '@highlight-run/next/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
@@ -11,6 +13,17 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+  experimental: {
+    instrumentationHook: true,
+    // serverComponentsExternalPackages: ['@highlight-run/node'], // highlight에서 API route instrumentation 적용할때 사용: https://www.highlight.io/docs/getting-started/fullstack-frameworks/next-js/app-router#api-route-instrumentation
+  },
+  webpack(config, options) {
+    if (options.isServer) {
+      config.ignoreWarnings = [{ module: /highlight-(run\/)?node/ }];
+    }
+
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withHighlightConfig(nextConfig);
