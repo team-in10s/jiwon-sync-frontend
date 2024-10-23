4c2b035c2dacfc972cb64846ee318c3a8d5e27f0
jean-lee-999 <jean@in10s.co>
2024-10-23 17:12:31 +0900

    style: remove margin right

--- app/app/components/apple-store-button.tsx
+++ app/app/components/apple-store-button.tsx
@@ -2,14 +2,8 @@ import Image from 'next/image';
 
 export default function AppleStoreButton() {
   return (
-    <button className="flex items-center rounded-lg bg-dimmed-70 px-4 py-2 font-medium text-white transition duration-300 hover:bg-gray-800">
-      <Image
-        src="/assets/new-landing/applekorea.png"
-        alt="App Store"
-        width={20}
-        height={20}
-        className="mr-2"
-      />
+    <button className="flex items-center gap-2 rounded-lg bg-dimmed-70 px-4 py-2 font-medium text-white transition duration-300 hover:bg-gray-800">
+      <Image src="/assets/new-landing/applekorea.png" alt="App Store" width={20} height={20} />
       App Store
     </button>
   );
