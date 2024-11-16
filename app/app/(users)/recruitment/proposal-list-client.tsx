33259962a02d5325f416491b4aa144012b29b5f2
jean-lee-999 <jean@in10s.co>
2024-11-16 20:30:41 +0900

    feat: pagination

--- app/app/(users)/recruitment/proposal-list-client.tsx
+++ app/app/(users)/recruitment/proposal-list-client.tsx
@@ -4,13 +4,15 @@ import { useState } from 'react';
 import { Email } from './types';
 import EmailBody from './email-body';
 import EmailInbox from './email-inbox';
+import { Pagination } from './pagination';
 
 interface ProposalListClientProps {
   emails: Email[];
+  totalEmails: number;
 }
 
 // eslint-disable-next-line react/prop-types
-const ProposalListClient: React.FC<ProposalListClientProps> = ({ emails }) => {
+const ProposalListClient: React.FC<ProposalListClientProps> = ({ emails, totalEmails }) => {
   const [selectedEmail, setSelectedEmail] = useState<Email | null>(null);
 
   const handleEmailClick = (email: Email) => {
@@ -19,11 +21,17 @@ const ProposalListClient: React.FC<ProposalListClientProps> = ({ emails }) => {
 
   return (
     <div className="flex h-full flex-col overflow-hidden rounded-lg bg-gray-600 shadow-md md:flex-row">
-      <EmailInbox
-        emails={emails}
-        selectedEmail={selectedEmail}
-        handleEmailClick={handleEmailClick}
-      />
+      <div
+        className="w-full overflow-y-auto border-r border-gray-700 md:w-1/3"
+        style={{ height: 'calc(100vh - 12rem)' }}
+      >
+        <EmailInbox
+          emails={emails}
+          selectedEmail={selectedEmail}
+          handleEmailClick={handleEmailClick}
+        />
+        <Pagination totalEmails={totalEmails} />
+      </div>
 
       <div className="flex w-full flex-col md:w-2/3">
         {selectedEmail ? (
--- app/app/(users)/recruitment/utils.ts
+++ app/app/(users)/recruitment/utils.ts
@@ -17,21 +17,45 @@ export function getEmailStatusMessage(email: Email): string {
 }
 
 export function getEmailReadStatus(email: Email): string {
-  if (email.read) {
-    const currentTime = new Date();
-    const emailTime = new Date(email.date);
-    const timeDiff = Math.floor((currentTime.getTime() - emailTime.getTime()) / (1000 * 60 * 60)); // 시간 단위로 계산
-    const remainingTime = 72 - timeDiff;
-    if (remainingTime > 0) {
-      return `응답 대기중 : ${remainingTime}시간 남음`;
-    } else {
-      return '응답 시간 초과';
-    }
+  // if (email.read) {
+  const currentTime = new Date();
+  const emailTime = new Date(email.date);
+  const timeDiff = Math.floor((currentTime.getTime() - emailTime.getTime()) / (1000 * 60 * 60)); // 시간 단위로 계산
+  const remainingTime = 72 - timeDiff;
+  if (remainingTime > 0) {
+    return `응답 대기중 : ${remainingTime}시간 남음`;
+  } else {
+    return '응답 시간 초과';
   }
-  return '';
+  // }
+  // return '';
 }
 
-// function formatDate(dateString: string): string {
-//   const date = new Date(dateString);
-//   return date.toLocaleDateString();
-// }
+export const calculateTotalPages = (totalEmails: number, emailsPerPage: number = 10): number => {
+  return Math.ceil(totalEmails / emailsPerPage);
+};
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  // If the total number of pages is 7 or less,
+  // display all pages without any ellipsis.
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  // If the current page is among the first 3 pages,
+  // show the first 3, an ellipsis, and the last 2 pages.
+  if (currentPage <= 3) {
+    return [1, 2, 3, '...', totalPages - 1, totalPages];
+  }
+
+  // If the current page is among the last 3 pages,
+  // show the first 2, an ellipsis, and the last 3 pages.
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  // If the current page is somewhere in the middle,
+  // show the first page, an ellipsis, the current page and its neighbors,
+  // another ellipsis, and the last page.
+  return [1, '...', currentPage - 1, currentPage, currentPage + 1, '...', totalPages];
+};
