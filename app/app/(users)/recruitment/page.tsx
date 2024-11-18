b48cda44419e10e79fd2a3553c986d248fdc4fdf
jean-lee-999 <jean@in10s.co>
2024-11-18 17:06:49 +0900

    refactor: 컴포넌트 분리

--- app/app/(users)/recruitment/page.tsx
+++ app/app/(users)/recruitment/page.tsx
@@ -1,60 +1,8 @@
-import { Email, ErrorResponse } from './types';
-import ProposalListClient from './proposal-list-client';
 import EmailSkeleton from './email-skeleton';
 import { Suspense } from 'react';
-import { getServerAuth } from '@/app/lib/server-auth';
+import ProposalList from './proposal-list';
 
-type TotalEmailsResponse = {
-  total_count: number;
-};
-
-type TotalEmailErrorResponse = {
-  detail: string;
-};
-
-async function fetchTotalEmails(): Promise<number> {
-  const { credentials } = getServerAuth();
-  const response = await fetch(`${process.env.API_BASE_URL}/scout/proposals/count`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Basic ${credentials}`,
-    },
-  });
-
-  if (!response.ok) {
-    const errorData: TotalEmailErrorResponse = await response.json();
-    console.error(errorData.detail);
-    throw new Error('Failed to fetch total emails');
-  }
-
-  const data: TotalEmailsResponse = await response.json();
-  return data.total_count;
-}
-
-async function fetchEmails(page = 0, size = 20, unreadOnly = false): Promise<Email[]> {
-  const { credentials } = getServerAuth();
-  const response = await fetch(
-    `${process.env.API_BASE_URL}/scout/proposals?page=${page}&size=${size}&unread_only=${unreadOnly}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${credentials}`,
-      },
-    }
-  );
-
-  if (!response.ok) {
-    const errorData: ErrorResponse = await response.json();
-    console.error(errorData);
-
-    throw new Error('Failed to fetch emails');
-  }
-
-  return response.json();
-}
-
-export default async function Index(props: {
+export default async function RecruitmentPage(props: {
   searchParams?: Promise<{
     page?: string;
   }>;
@@ -72,29 +20,4 @@ export default async function Index(props: {
       </Suspense>
     </div>
   );
-}
-
-async function ProposalList({ currentPage }: { currentPage: number }) {
-  // let totalEmails = 0;
-  // let allEmails: Email[] = [];
-
-  // try {
-  //   const [fetchedTotalEmails, fetchedEmails] = await Promise.all([
-  //     fetchTotalEmails(),
-  //     fetchEmails(currentPage - 1, 20),
-  //   ]);
-
-  //   totalEmails = fetchedTotalEmails;
-  //   allEmails = fetchedEmails;
-  // } catch (error) {
-  //   console.error('Error fetching emails or total emails:', error);
-  //   // totalEmails remains 0 and allEmails remains an empty array
-  // }
-
-  const [fetchedTotalEmails, fetchedEmails] = await Promise.all([
-    fetchTotalEmails(),
-    fetchEmails(currentPage - 1, 20),
-  ]);
-
-  return <ProposalListClient emails={fetchedEmails} totalEmails={fetchedTotalEmails} />;
 }
