4ec4cc73510115e1fe38a80abecd16aa9b14077b
jean-lee-999 <jean@in10s.co>
2024-11-20 15:06:17 +0900

    refactor: 유틸 함수 위치 변경

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -139,18 +139,3 @@ export function formatDate(dateString: string) {
     minute: '2-digit',
   });
 }
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const convertIIFEString = <T extends (...args: any[]) => any>(
-  fn: T,
-  ...args: Parameters<T>
-): string => {
-  // Convert function to string
-  const fnString = fn.toString();
-
-  // Convert arguments to JSON string to embed in the function call
-  const argsString = args.map((arg) => JSON.stringify(arg)).join(', ');
-
-  // Wrap function and call it with arguments as an IIFE
-  return `(${fnString})(${argsString})`;
-};
