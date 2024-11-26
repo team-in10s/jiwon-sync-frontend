6a62fc6da2c1cc8f1e65c39508d6c7c654e0f0b6
jean-lee-999 <jean@in10s.co>
2024-11-26 17:00:10 +0900

    feat: 스크래핑 이력서 조회 화면 작업 중 (자기소개)

---
+++ app/app/(users)/fake-resume-by-platforms/introduction-card.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ResumeCardWrapper from './resume-card-wrapper';
+
+type IntroductionProps = {
+  data: {
+    platform: string;
+    isSelected: boolean;
+    content: string;
+  }[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick: (data: any) => void;
+};
+
+const IntroductionCard: React.FC<IntroductionProps> = ({ data, onClick }) => (
+  <>
+    {data
+      .filter((item) => item.isSelected)
+      .map((item) => (
+        <ResumeCardWrapper
+          platform={item.platform}
+          borderColor="border-black"
+          onClick={() => onClick(item)}
+          key={item.platform}
+        >
+          <p>{item.content}</p>
+        </ResumeCardWrapper>
+      ))}
+  </>
+);
+
+export default IntroductionCard;
