7e3cc90f5273065bf6f7323ac2f57310fe283162
jean-lee-999 <jean@in10s.co>
2024-11-26 17:01:46 +0900

    feat: 스크래핑 이력서 조회 화면 작업 중 (수상)

---
+++ app/app/(users)/fake-resume-by-platforms/award-card.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ResumeCardWrapper from './resume-card-wrapper';
+
+type AwardProps = {
+  data: {
+    platform: string;
+    isSelected: boolean;
+    name: string;
+    date: string;
+    description: string;
+    awardedBy: string;
+  }[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick: (data: any) => void;
+};
+
+const AwardCard: React.FC<AwardProps> = ({ data, onClick }) => (
+  <>
+    {data
+      .filter((item) => item.isSelected)
+      .map((item) => (
+        <ResumeCardWrapper
+          platform={item.platform}
+          borderColor="border-green-500"
+          onClick={() => onClick(item)}
+          key={item.platform}
+        >
+          <div>
+            <p className="font-bold">{item.name}</p>
+            <p>{item.awardedBy}</p>
+            <p>{item.date}</p>
+            <p>{item.description}</p>
+          </div>
+        </ResumeCardWrapper>
+      ))}
+  </>
+);
+
+export default AwardCard;
