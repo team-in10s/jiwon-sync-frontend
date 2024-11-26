e4f6136a6e46d7ed59a98a5542d30bc68c401eba
jean-lee-999 <jean@in10s.co>
2024-11-26 19:28:13 +0900

    style: 화면 너비에 따라 패딩값 조정

--- app/app/(auth)/layout.tsx
+++ app/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-4 py-28">
+    <main className="flex min-h-screen flex-col items-center justify-center px-4 py-12 sm:py-28">
       {children}
     </main>
   );
