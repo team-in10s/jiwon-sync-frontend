6c8831f76ae71077802eb723198336530c0a6dcc
hyungi <hkgo6040@gmail.com>
2024-12-02 15:50:48 +0900

    inblog subDir

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/api/platform/:path*',
-        destination: `${process.env.API_BASE_URL}/platform/:path*`,
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/blog",
+          destination: "https://inblog.ai/jiwon-team",
+        },
+        {
+          source: "/blog/:path*",
+          destination: "https://inblog.ai/jiwon-team/:path*",
+        },
+        {
+          source: "/robots.txt",
+          destination: "https://inblog.ai/jiwon-team/robots.txt",
+        },
+        {
+          source: "/_inblog/:path*",
+          destination: "https://inblog.ai/jiwon-team/_inblog/:path*",
+        },
+      ],
+      afterFiles: [
+        {
+          source: '/api/platform/:path*',
+          destination: `${process.env.API_BASE_URL}/platform/:path*`,
+        },
+      ],
+    };
   },
   compiler: {
     // removeConsole: process.env.NODE_ENV === 'production',
