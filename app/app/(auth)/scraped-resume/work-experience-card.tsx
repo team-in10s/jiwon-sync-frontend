3407158d431d22c798a6241b41a2f59d31ddd9e5
jean-lee-999 <jean@in10s.co>
2024-12-02 19:12:27 +0900

    style

--- app/app/(auth)/scraped-resume/work-experience-card.tsx
+++ app/app/(auth)/scraped-resume/work-experience-card.tsx
@@ -20,23 +20,36 @@ const WorkExperienceCard: React.FC<WorkExperienceProps> = ({ data, onClick }) =>
           onClick={() => onClick(item)}
           key={item.platform}
         >
-          <div className="flex items-center">
-            <div className="mr-4 size-14 rounded-full bg-gray-200"></div>
-            <div>
-              <p className="font-bold">{item.jobTitle}</p>
-              <p>{item.companyName}</p>
-              <p>{item.companySize}</p>
-              <p>{item.companyType}</p>
-              <p>{item.department}</p>
-              <p>{item.industryType}</p>
-              <p>{item.isCurrent}</p>
-              <p>{item.salary}</p>
-              <p>{item.workType}</p>
-              <p>
-                {item.startYear}/{item.startMonth} - {item.endYear}/{item.endMonth}
-              </p>
-              <p>{item.responsibilities}</p>
+          <div>
+            <div className="mb-1.5 flex items-center">
+              <div className="mr-4 size-14 rounded bg-gray-200"></div>
+              <div>
+                <p className="font-semibold">{item.jobTitle}</p>
+                <p className="font-semibold">{item.companyName}</p>
+                <p>{item.department}</p>
+                <div>
+                  <span>
+                    {item.startYear}.{item.startMonth}
+                  </span>
+                  <span> - </span>
+                  {item.isCurrent ? (
+                    <span>재직 중</span>
+                  ) : (
+                    <span>
+                      {item.endYear}.{item.endMonth}
+                    </span>
+                  )}
+                </div>
+              </div>
             </div>
+
+            {/* <p>{item.companySize}</p> */}
+            {/* <p>{item.companyType}</p> */}
+            {/* <p>{item.industryType}</p> */}
+            {/* <p>{item.salary}</p> */}
+            {/* <p>{item.workType}</p> */}
+
+            <p>{item.responsibilities}</p>
           </div>
         </ResumeCardWrapper>
       ))}
